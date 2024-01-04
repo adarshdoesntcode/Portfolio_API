b778d9be1fde86f2c51cb3e97229c084ca4374bb
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-01-04 17:48:56 +0545

    added cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../config.env") });
 
 const express = require("express");
+var cors = require("cors");
 
 const spotifyRouter = require("./routers/spotify");
 
 require("./db/mongoose");
 
 const app = express();
+app.use(cors());
 app.use(spotifyRouter);
 
 module.exports = app;
