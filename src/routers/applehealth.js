63619d53d653fa9976b4810882739e61e45f6650
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-01-06 12:50:07 +0545

    scopes updates

--- src/routers/applehealth.js
+++ src/routers/applehealth.js
@@ -14,9 +14,7 @@ healthRouter.get("/health/steps", (req, res) => {
     "https://adarshportfolio.cyclic.app/steps"
   );
 
-  const scopes = [
-    "https://www.googleapis.com/auth/fitness.activity.read profile email openid",
-  ];
+  const scopes = ["https://www.googleapis.com/auth/fitness.activity.read"];
 
   const url = oauth2Client.generateAuthUrl({
     access_type: "offline",
