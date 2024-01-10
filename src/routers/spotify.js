fc077be2d4d3a79e13c88153c89ee1bc3c6239b7
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-01-10 23:00:08 +0545

    spotify api mod

--- src/routers/spotify.js
+++ src/routers/spotify.js
@@ -11,15 +11,11 @@ let SPOTIFY_REFRESH_TOKEN;
 const tokens = async () => {
   SPOTIFY_ACCESS_TOKEN = await client.get("SPOTIFY_ACCESS_TOKEN");
   SPOTIFY_REFRESH_TOKEN = await client.get("SPOTIFY_REFRESH_TOKEN");
-
-  console.log(SPOTIFY_ACCESS_TOKEN);
 };
 
 tokens();
 
 spotifyRouter.get("/spotify/get-player-state", async (req, res) => {
-  console.log("getting player state");
-  // console.log("first access token", process.env.ACCESS_TOKEN);
   try {
     const response = await fetch(
       "https://api.spotify.com/v1/me/player/currently-playing",
@@ -61,7 +57,6 @@ spotifyRouter.get("/spotify/get-player-state", async (req, res) => {
 });
 
 spotifyRouter.get("/spotify/recently-played", async (req, res) => {
-  console.log("recently played");
   try {
     const response = await fetch(
       "https://api.spotify.com/v1/me/player/recently-played",
@@ -89,11 +84,6 @@ spotifyRouter.get("/spotify/recently-played", async (req, res) => {
 });
 
 spotifyRouter.get("/spotify/refresh-token", function (req, res) {
-  console.log("refreshing token routee");
-
-  console.log(req.headers["user-agent"]);
-  console.log(req.headers["accept"]);
-
   const authOptions = {
     url: "https://accounts.spotify.com/api/token",
     headers: {
@@ -114,13 +104,9 @@ spotifyRouter.get("/spotify/refresh-token", function (req, res) {
 
   request.post(authOptions, async function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      process.env.ACCESS_TOKEN = body.access_token;
       await client.set("SPOTIFY_ACCESS_TOKEN", body.access_token);
-      if (req.headers["x-cyclic"] !== "cron") {
-        res.redirect("/spotify/get-player-state");
-      } else {
-        res.status(200);
-      }
+
+      res.redirect("/spotify/get-player-state");
     }
   });
 });
