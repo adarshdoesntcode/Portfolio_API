8cc94051a6cdf3c3c68a646e571a17f6df1581c7
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-01-14 14:00:31 +0545

    redis cache for strava

--- src/routers/strava.js
+++ src/routers/strava.js
@@ -14,7 +14,7 @@ const getTokens = async (req, res, next) => {
   next();
 };
 
-stravaRouter.get("/strava/get-activities", getTokens, async (req, res) => {
+stravaRouter.post("/strava/get-activities", getTokens, async (req, res) => {
   try {
     const response = await fetch(
       "https://www.strava.com/api/v3/athlete/activities",
@@ -29,6 +29,14 @@ stravaRouter.get("/strava/get-activities", getTokens, async (req, res) => {
     if (response.status !== 401) {
       const data = await response.json();
 
+      await client.hset("STRAVA_DATA", {
+        distance: data[0].distance,
+        moving_time: data[0].moving_time,
+        type: data[0].type,
+        average_speed: data[0].average_speed,
+        start_date_local: data[0].start_date_local,
+      });
+
       res.status(200).json({
         distance: data[0].distance,
         moving_time: data[0].moving_time,
@@ -39,6 +47,15 @@ stravaRouter.get("/strava/get-activities", getTokens, async (req, res) => {
     }
   } catch (e) {
     console.log(e);
+  }
+});
+
+stravaRouter.get("/strava/get-activities", async (req, res) => {
+  try {
+    const data = await client.hgetall("STRAVA_DATA");
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
   }
 });
 
