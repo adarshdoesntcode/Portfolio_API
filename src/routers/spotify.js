c77b7a7898eab3b8c47386d62de13a43a13b1ef5
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-01-14 11:28:40 +0545

    spotify optimization with middleware

--- src/routers/spotify.js
+++ src/routers/spotify.js
@@ -5,38 +5,36 @@ const Redis = require("ioredis");
 const spotifyRouter = new express.Router();
 
 const client = new Redis(process.env.REDIS_URL);
-let SPOTIFY_ACCESS_TOKEN;
-let SPOTIFY_REFRESH_TOKEN;
 
-const tokens = async () => {
-  SPOTIFY_ACCESS_TOKEN = await client.get("SPOTIFY_ACCESS_TOKEN");
-  SPOTIFY_REFRESH_TOKEN = await client.get("SPOTIFY_REFRESH_TOKEN");
+const getTokens = async (req, res, next) => {
+  let SPOTIFY_ACCESS_TOKEN = await client.get("SPOTIFY_ACCESS_TOKEN");
+
+  if (!SPOTIFY_ACCESS_TOKEN) {
+    SPOTIFY_ACCESS_TOKEN = await refreshToken();
+  }
+  res.locals.SPOTIFY_ACCESS_TOKEN = SPOTIFY_ACCESS_TOKEN;
+  next();
 };
 
-tokens();
-
-spotifyRouter.get("/spotify/get-player-state", async (req, res) => {
+spotifyRouter.get("/spotify/get-player-state", getTokens, async (req, res) => {
   try {
     const response = await fetch(
       "https://api.spotify.com/v1/me/player/currently-playing",
       {
         method: "GET",
         headers: {
-          Authorization: "Bearer " + SPOTIFY_ACCESS_TOKEN,
+          Authorization: "Bearer " + res.locals.SPOTIFY_ACCESS_TOKEN,
           "Content-Type": "application/json",
         },
       }
     );
 
-    if (response.status === 401) {
-      res.redirect("/spotify/refresh-token");
-    }
-
     if (response.status === 204) {
+      console.log("204");
       res.redirect("/spotify/recently-played");
     }
 
-    if (response.status !== 401 && response.status !== 204) {
+    if (response.status !== 204) {
       const data = await response.json();
       if (data.currently_playing_type === "episode") {
         res.redirect("/spotify/recently-played");
@@ -56,19 +54,20 @@ spotifyRouter.get("/spotify/get-player-state", async (req, res) => {
   }
 });
 
-spotifyRouter.get("/spotify/recently-played", async (req, res) => {
+spotifyRouter.get("/spotify/recently-played", getTokens, async (req, res) => {
   try {
     const response = await fetch(
       "https://api.spotify.com/v1/me/player/recently-played",
       {
         method: "GET",
         headers: {
-          Authorization: "Bearer " + SPOTIFY_ACCESS_TOKEN,
+          Authorization: "Bearer " + res.locals.SPOTIFY_ACCESS_TOKEN,
           "Content-Type": "application/json",
         },
       }
     );
     const data = await response.json();
+
     const recentTrack = data.items[0].track;
     res.status(200).json({
       trackTitle: recentTrack.name,
@@ -83,32 +82,50 @@ spotifyRouter.get("/spotify/recently-played", async (req, res) => {
   }
 });
 
-spotifyRouter.get("/spotify/refresh-token", function (req, res) {
+const refreshToken = async () => {
+  let accessToken;
+
   const authOptions = {
-    url: "https://accounts.spotify.com/api/token",
+    method: "POST",
     headers: {
-      method: "POST",
-      "content-type": "application/x-www-form-urlencoded",
+      "Content-Type": "application/x-www-form-urlencoded",
       Authorization:
         "Basic " +
-        new Buffer.from(
+        Buffer.from(
           process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET_ID
         ).toString("base64"),
     },
-    form: {
+    body: new URLSearchParams({
       grant_type: "refresh_token",
-      refresh_token: SPOTIFY_REFRESH_TOKEN,
-    },
-    json: true,
+      refresh_token: process.env.REFRESH_TOKEN,
+    }),
   };
 
-  request.post(authOptions, async function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      await client.set("SPOTIFY_ACCESS_TOKEN", body.access_token);
+  try {
+    const response = await fetch(
+      "https://accounts.spotify.com/api/token",
+      authOptions
+    );
+    const body = await response.json();
 
-      res.redirect("/spotify/get-player-state");
+    if (!response.ok) {
+      throw new Error(`Failed to refresh token: ${body.error}`);
     }
-  });
-});
+
+    accessToken = body.access_token;
+    await client.set(
+      "SPOTIFY_ACCESS_TOKEN",
+      body.access_token,
+      "EX",
+      body.expires_in - 5
+    );
+  } catch (error) {
+    console.error("Error refreshing token:", error.message);
+  }
+
+  return accessToken;
+};
+
+refreshToken();
 
 module.exports = spotifyRouter;
